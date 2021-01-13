539f4b5a7c2d1426b977c3487b0cbcfe7aa2611e
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-01-13 14:59:22 -0500

    add secureDomains

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Application = require('@waline/vercel');
 
 module.exports = Application({
-  async postSave(comment) {
-    //do what ever you want after save comment
-  }
+  secureDomains: 'njzjz.win',
+  forbiddenWords: [
+    '习近平',
+    '毛泽东'
+  ]
 });
